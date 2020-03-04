501eda25800056d2a01916df68e6d080b2c56fea
Berman <VBerman@users.noreply.github.com>
2020-03-04 21:02:02 +0300

    Под серверную структуру
    Читай редми

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>CinemaStar</title>
+    <link rel="stylesheet" href=css/cssMM.css>
   </head>
   <body>
     <h1 class="">Кинотеатр "Синема Стар"</h1>
